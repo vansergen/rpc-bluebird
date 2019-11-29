59f8970425eabe9a05a31c3109c7aa4a91a06e1a
Sergey Bakulin <vansergen@gmail.com>
2019-11-29 12:15:29 -0500

    fix: add missing return types

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import * as request from "request-promise";
+import { Cookie, CookieJar } from "request";
 
 export type RPCOtpions = request.RequestPromiseOptions | request.Options;
 
@@ -9,43 +10,49 @@ export class RPC {
     this._rpoptions = options;
   }
 
-  get(options: RPCOtpions = {}) {
+  get(options: RPCOtpions = {}): request.RequestPromise<any> {
     return this.request({ ...options, method: "GET" });
   }
 
-  post(options: RPCOtpions = {}) {
+  post(options: RPCOtpions = {}): request.RequestPromise<any> {
     return this.request({ ...options, method: "POST" });
   }
 
-  put(options: RPCOtpions = {}) {
+  put(options: RPCOtpions = {}): request.RequestPromise<any> {
     return this.request({ ...options, method: "PUT" });
   }
 
-  patch(options: RPCOtpions = {}) {
+  patch(options: RPCOtpions = {}): request.RequestPromise<any> {
     return this.request({ ...options, method: "PATCH" });
   }
 
-  delete(options: RPCOtpions = {}) {
+  delete(options: RPCOtpions = {}): request.RequestPromise<any> {
     return this.request({ ...options, method: "DELETE" });
   }
 
-  head(options: RPCOtpions = {}) {
+  head(options: RPCOtpions = {}): request.RequestPromise<any> {
     return this.request({ ...options, method: "HEAD" });
   }
 
-  options(options: RPCOtpions = {}) {
+  options(options: RPCOtpions = {}): request.RequestPromise<any> {
     return this.request({ ...options, method: "OPTIONS" });
   }
 
-  request(options: RPCOtpions = {}) {
+  request(options: RPCOtpions = {}): request.RequestPromise<any> {
     return this.defaults(RPC.prepareOptions(options, this._rpoptions));
   }
 
-  static cookie(key: string, value: string) {
+  /**
+   * Create a new cookie
+   */
+  static cookie(key: string, value: string): Cookie | undefined {
     return request.cookie(key + "=" + value);
   }
 
-  static jar(cookieStore?: any) {
+  /**
+   * Create a new cookie jar
+   */
+  static jar(cookieStore?: any): CookieJar {
     return request.jar(cookieStore);
   }
 
